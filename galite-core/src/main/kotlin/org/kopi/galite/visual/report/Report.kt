b36c737807762465f84f5074de32dc7e5191659a
Hichem Fazai <hf.fazai@gmail.com>
2020-08-18 11:50:54 +0100

    Supports Report (#2)

---
+++ galite-core/src/main/kotlin/org/kopi/galite/visual/report/Report.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2013-2020 kopiLeft Services SARL, Tunis TN
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.kopi.galite.visual.report
+
+import org.kopi.galite.domain.Domain
+import org.kopi.galite.visual.field.Field
+
+/**
+ * Represents a report that contains fields [fields] and displays a table of [lines].
+ */
+open class Report {
+  /** Report's fields */
+  val fields = mutableListOf<Field<*>>()
+  /** Report's data lines */
+  private val lines = mutableListOf<MutableMap<Field<*>, Any>>()
+
+  /**
+   * creates and returns a field. It uses [init] method to initialize the field.
+   *
+   * @param domain  the domain of the field
+   * @param init    initialization method
+   * @return a field.
+   */
+  fun <T: Comparable<T>> field(domain: Domain<T>, init: Field<T>.() -> Unit) : Field<T> {
+    val field = Field(domain)
+    field.init()
+    fields.add(field)
+    return field
+  }
+
+  /**
+   * Sets a mapping between the values that the domain can take
+   * and a corresponding text to be displayed in a [Field].
+   *
+   * @param field the field
+   * @param value the field's value
+   */
+  operator fun <T: Comparable<T>> set(field: Field<T>, value: T) {
+    if(field in fields) {
+      lines.last().putIfAbsent(field, value)
+    }
+  }
+
+  /**
+   * Adds a line to the report.
+   *
+   * @param init initializes the line with values
+   */
+  fun add(init: Report.() -> Unit) {
+    val map = mutableMapOf<Field<*>, Any>()
+    lines.add(map)
+    init()
+  }
+
+  /**
+   * Returns line number [lineNumber].
+   *
+   * @param lineNumber line's number
+   */
+  fun getLine(lineNumber: Int): MutableMap<Field<*>, Any> = lines[lineNumber]
+
+  /**
+   * Returns lines of data for a specific [field].
+   *
+   * @param field the field
+   */
+  fun getLinesForField(field: Field<*>) = lines.map { it[field] }
+}
---
+++ galite-tests/src/test/kotlin/org/kopi/galite/tests/visual/report/ReportTests.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2013-2020 kopiLeft Services SARL, Tunis TN
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.kopi.galite.tests.visual.report
+
+import org.junit.Test
+import org.kopi.galite.domain.Domain
+import org.kopi.galite.visual.report.Report
+import kotlin.test.assertEquals
+
+class ReportTests {
+
+  /**
+   * Tests that fields has been registered in the report.
+   */
+  @Test
+  fun reportFieldsTest() {
+    val report = SimpleReport()
+
+    assertEquals(report.fields[0], report.field1)
+    assertEquals(report.fields[1], report.field2)
+  }
+
+  /**
+   * Tests that fields has been registered in the report.
+   */
+  @Test
+  fun reportDataTest() {
+    val report = SimpleReport()
+
+    val lines = report.getLinesForField(report.field1)
+    assertEquals(listOf("test1", "test2"), lines)
+
+    val firstLine = report.getLine(0)
+    assertEquals(mapOf(report.field1 to "test1", report.field2 to 64L), firstLine)
+
+    val secondLine = report.getLine(1)
+    assertEquals(mapOf(report.field1 to "test2", report.field2 to 32L), secondLine)
+  }
+
+  /**
+   * Simple Report with two fields.
+   */
+  class SimpleReport : Report() {
+    val field1 = field(StringTestType()) {
+      label = "field1"
+    }
+    val field2 = field(LongTestType()) {
+      label = "field2"
+    }
+
+    init {
+      add {
+        this[field1] = "test1"
+        this[field2] = 64L
+      }
+      add {
+        this[field1] = "test2"
+        this[field2] = 32L
+      }
+    }
+  }
+
+  class StringTestType : Domain<String>(5) {
+    override val values = code {
+      this["cde1"] = "test1"
+    }
+  }
+
+  class LongTestType : Domain<Long>(5) {
+    override val values = code {
+      this["cde1"] = 1
+    }
+  }
+}
+
