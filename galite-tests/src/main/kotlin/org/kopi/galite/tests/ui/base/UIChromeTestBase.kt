f21c11709753cb3442c7150839a6990d3ecc43c0
Hichem Fazai <hf.fazai@gmail.com>
2020-09-03 14:59:58 +0100

    adding UI tests utilities (#11)
    * adding UI tests utilities
    
    * code style fix
    
    * fix findElementsByTag
    
    * fix findElementsByTag

---
+++ galite-tests/src/main/kotlin/org/kopi/galite/tests/ui/base/UIChromeTestBase.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2013-2020 kopiLeft Services SARL, Tunis TN
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.kopi.galite.tests.ui.base
+
+import io.github.bonigarcia.wdm.WebDriverManager
+
+import org.openqa.selenium.chrome.ChromeDriver
+
+/**
+ * The high level class for all classes containing UI tests on Chrome
+ */
+open class UIChromeTestBase : UITestBase() {
+  override val driver = ChromeDriver()
+
+  override fun setupTest() {
+    WebDriverManager.chromedriver().setup()
+    super.setupTest()
+  }
+}
