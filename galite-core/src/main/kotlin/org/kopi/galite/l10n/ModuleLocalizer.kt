450f4165248782f3dcc7b80ff56fb9981865541d
h-haddad <houssem.haddad@ensi-uma.tn>
2020-09-15 11:11:26 +0100

    Adding l10n classes

---
+++ galite-core/src/main/kotlin/org/kopi/galite/l10n/ModuleLocalizer.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2013-2020 kopiLeft Services SARL, Tunis TN
+ * Copyright (c) 1990-2020 kopiRight Managed Solutions GmbH, Wien AT
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.kopi.galite.l10n
+
+import org.jdom2.Document
+import org.jdom2.Element
+import org.kopi.galite.util.base.InconsistencyException
+
+/**
+ * Implements an actor localizer.
+ * @param             document        the document containing the actor localization
+ * @param             ident           the identifier of the actor localization
+ */
+class ModuleLocalizer(document: Document, ident: String) {
+  // ----------------------------------------------------------------------
+  // ACCESSORS
+  // ----------------------------------------------------------------------
+  /**
+   * Returns the value of the label attribute.
+   */
+  fun getLabel(): String = self.getAttributeValue("label")
+
+  /**
+   * Returns the value of the help attribute.
+   */
+  fun getHelp(): String = self.getAttributeValue("help")
+
+  // ----------------------------------------------------------------------
+  // DATA MEMBERS
+  // ----------------------------------------------------------------------
+  private val self: Element
+
+  // ----------------------------------------------------------------------
+  // CONSTRUCTOR
+  // ----------------------------------------------------------------------
+  init {
+    val root: Element = document.rootElement
+
+    if (root.name != "modules") {
+      throw InconsistencyException("bad root element $root")
+    }
+    self = Utils.lookupChild(root, "module", "ident", ident)
+  }
+}
