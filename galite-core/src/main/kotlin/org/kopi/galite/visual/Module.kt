2ec8dae815f58e993b8385f0a33afd3c61f29e1f
hfazai <hf.fazai@gmail.com>
2020-11-16 09:14:31 +0100

    allow to declare form as an object

--- galite-core/src/main/kotlin/org/kopi/galite/visual/Module.kt
+++ galite-core/src/main/kotlin/org/kopi/galite/visual/Module.kt
@@ -19,6 +19,7 @@
 package org.kopi.galite.visual
 
 import org.kopi.galite.base.Image
+import org.kopi.galite.common.Window
 import org.kopi.galite.db.DBContext
 import org.kopi.galite.l10n.LocalizationManager
 import org.kopi.galite.l10n.ModuleLocalizer
@@ -136,7 +137,8 @@ class Module(val id: Int,
   companion object {
     fun getExecutable(objectName: String?): Executable {
       return try {
-        Class.forName(objectName).newInstance() as Executable
+        (Class.forName(objectName).kotlin.objectInstance as? Window)?.model
+                ?: Class.forName(objectName).newInstance() as Executable
       } catch (iae: IllegalAccessException) {
         throw VRuntimeException(iae)
       } catch (ie: InstantiationException) {
