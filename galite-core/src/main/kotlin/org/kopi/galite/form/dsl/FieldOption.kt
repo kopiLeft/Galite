137e8100e9a9911f383fb449cb7aae244e660b0f
hfazai <hf.fazai@gmail.com>
2020-11-30 15:49:29 +0100

    VConstants.FDO_DO_NOT_ERASE_ON_LOOKUP is the value of NO_DELETE_ON_UPDATE option

--- galite-core/src/main/kotlin/org/kopi/galite/form/dsl/FieldOption.kt
+++ galite-core/src/main/kotlin/org/kopi/galite/form/dsl/FieldOption.kt
@@ -18,24 +18,24 @@ package org.kopi.galite.form.dsl
 
 import org.kopi.galite.form.VConstants
 
-enum class FieldOption(val value: Int){
+enum class FieldOption(val value: Int) {
   /**
    * If this option is used, characters typed in the field will not be displayed and a star(*) will be
    * displayed instead, this option is useful for password fields.
    */
-  NOECHO (VConstants.FDO_NOECHO),
+  NOECHO(VConstants.FDO_NOECHO),
 
   /**
    * This option makes it impossible to change the data of the field or to overwrite it.
    */
-  NOEDIT (VConstants.FDO_NOEDIT),
+  NOEDIT(VConstants.FDO_NOEDIT),
 
   /**
    * This option adds two opposed arrows icons(up and down) just before the field,
    * clicking on the icon changes the way data are sorted in the field,
    * you can click the icon three times to have ascending sort, descending sort and default sort.
    */
-  SORTABLE (VConstants.FDO_SORT),
+  SORTABLE(VConstants.FDO_SORT),
 
   /**
    * This option make the field transient, meaning that the system can no trail it,
@@ -43,31 +43,31 @@ enum class FieldOption(val value: Int){
    * the field will not be able to backup or roll-back to its original value,
    * besides this option makes changes ignored for this field.
    */
-  TRANSIENT (VConstants.FDO_TRANSIENT),
+  TRANSIENT(VConstants.FDO_TRANSIENT),
 
   /**
    * If the field is a lookup is a column of a lookup table, using this option prevent
    * the system to clear the field when inserting new rows or updating rows.
    */
-  NO_DELETE_ON_UPDATE (VConstants.ALG_RIGHT),
+  NO_DELETE_ON_UPDATE(VConstants.FDO_DO_NOT_ERASE_ON_LOOKUP),
 
   /**
    * If the block is in detailed, using this option on a field make it invisible in the detail.
    */
-  NO_DETAIL (VConstants.FDO_NODETAIL),
+  NO_DETAIL(VConstants.FDO_NODETAIL),
 
   /**
    * If the block is multiple, using this option on a field exclude it from the chart.
    */
-  NO_CHART (VConstants.FDO_NOCHART),
+  NO_CHART(VConstants.FDO_NOCHART),
 
   /**
    * Whatever the string you input, this option will make kopi transform it to capital letters.
    */
-  QUERY_UPPER (VConstants.FDO_SEARCH_UPPER),
+  QUERY_UPPER(VConstants.FDO_SEARCH_UPPER),
 
   /**
    * the opposite of the previous option it transform strings to lower case.
    */
-  QUERY_LOWER (VConstants.FDO_SEARCH_LOWER)
+  QUERY_LOWER(VConstants.FDO_SEARCH_LOWER)
 }
