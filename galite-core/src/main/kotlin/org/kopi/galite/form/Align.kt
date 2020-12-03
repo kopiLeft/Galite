11a0169bb2ce9890f0eeef3ace1fe3214f942f3e
SNsir <nsirsarah24@gmail.com>
2020-12-03 11:47:22 +0100

    delete unused class Align

--- galite-core/src/main/kotlin/org/kopi/galite/form/Align.kt
+++
@@ -1,9 +0,0 @@
-package org.kopi.galite.form
-
-enum class Align(val value: Int){
-   DEFAULT (VConstants.ALG_DEFAULT),
-   CENTER (VConstants.ALG_CENTER),
-   LEFT (VConstants.ALG_LEFT),
-   RIGHT (VConstants.ALG_RIGHT),
-
-}
