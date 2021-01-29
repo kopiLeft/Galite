26c066ffa8501d752add9f522ac2f8bcf2d385c4
h-haddad <houssem.haddad@ensi-uma.tn>
2021-01-29 14:28:53 +0100

    Improvement of VActorPanel

--- galite-core/src/main/kotlin/org/kopi/galite/ui/vaadin/window/VActorPanel.kt
+++ galite-core/src/main/kotlin/org/kopi/galite/ui/vaadin/window/VActorPanel.kt
@@ -17,44 +17,24 @@
  */
 package org.kopi.galite.ui.vaadin.window
 
-import java.util.LinkedList
-
 import com.vaadin.flow.component.Component
-import com.vaadin.flow.component.HasEnabled
-import com.vaadin.flow.component.HasSize
-import com.vaadin.flow.component.html.Div
+import com.vaadin.flow.component.dependency.CssImport
+import com.vaadin.flow.component.menubar.MenuBar
 
-import org.kopi.galite.ui.vaadin.actor.VActorsNavigationPanel
 import org.kopi.galite.ui.vaadin.base.Styles
 
 /**
  * The actor components container.
  */
-class VActorPanel : Div(), HasSize {
-
-  //---------------------------------------------------
-  // DATA MEMBERS
-  //---------------------------------------------------
-  private val actorsContainer = Div()
-  private val menu = Div()
-  private val actorsPane = Div()
-  private val actors = LinkedList<Component>()
-  private val moreActors = VMoreActors()
-  private val actorsWidths = HashMap<Component, Int>()
-  private val actorsNavigationItem = VActorsRootNavigationItem()
-
+@CssImport("./styles/galite/vActorPanelStyles.css")
+class VActorPanel : MenuBar() {
   //---------------------------------------------------
   // CONSTRUCTOR
   //---------------------------------------------------
   init {
     className = Styles.WINDOW_VIEW_ACTORS
     this.setId("actors")
-    menu.setId("menu")
-    actorsContainer.className = "actors-slide-menu"
-    actorsPane.add(actorsContainer)
-    add(menu)
-    add(actorsPane)
-    menu.add(actorsNavigationItem)
+    this.setWidthFull()
   }
 
   //---------------------------------------------------
@@ -65,147 +45,7 @@ class VActorPanel : Div(), HasSize {
    * @param actor The actor to be added.
    */
   fun addActor(actor: Component) {
-    actors.add(actor)
-    actorsContainer.add(actor)
-  }
-
-  /**
-   * Adds the actors menu to be shown.
-   * @param panel The menu to be shown.
-   */
-  fun addActorsNavigationPanel(panel: VActorsNavigationPanel) {
-    actorsNavigationItem.setActorsNavigationPanel(panel)
-  }
-
-  /**
-   * Removes an actor from this panel.
-   * @param actor The actor to be removed.
-   */
-  protected fun removeActor(actor: Component) {
-    actors.remove(actor)
-    actorsContainer.remove(actor)
-  }
-
-  /**
-   * Returns the total actor width.
-   * @return The total actor width.
-   */
-  protected fun getActorsWidth(): Int {
-    if (actors == null || actors.isEmpty()) {
-      return 0
-    }
-    var width = 0
-
-    for (actor in actors) {
-      if (actor != null) {
-        width += actor.element.getAttribute("Width").toInt()
-        if (!actorsWidths.containsKey(actor)) {
-          actorsWidths[actor] = actor.element.getAttribute("Width").toInt()
-        }
-      }
-    }
-    return width
-  }
-
-  /**
-   * Renders the actor panel according to the total actor width.
-   * @param width The browser window width.
-   */
-  protected fun render(width: Int) {
-    if (getActorsWidth() > width) {
-      //TODO
-    } else {
-      restoreExtraActors(width)
-    }
-  }
-
-  /**
-   * Renders the more actors list.
-   * @param width The browser window width.
-   */
-  protected fun <T> addMoreActors(width: Int) where T : Component?, T : HasEnabled? {
-    val extraActors: LinkedList<Component> = getExtraActors(width)
-
-    if (!extraActors.isEmpty()) {
-      if (moreActors.isEmpty()) {
-        for (actor in extraActors) {
-          if (actor != null) {
-            moreActors.addActor(actor as T)
-          }
-        }
-      } else {
-        val allActors: LinkedList<Component> = LinkedList<Component>(extraActors)
-
-        allActors.addAll(moreActors.getActors())
-        moreActors.clear()
-        for (actor in allActors) {
-          if (actor != null) {
-            moreActors.addActor(actor as T)
-          }
-        }
-      }
-      if (!actors.contains(moreActors)) {
-        addActor(moreActors)
-      }
-    }
-  }
-
-  /**
-   * Returns the list of actors that has exceeded the total browser window width.
-   * @param width The browser window width.
-   * @return The of the extra actors.
-   */
-  protected fun getExtraActors(width: Int): LinkedList<Component> {
-    val extraActors: LinkedList<Component> = LinkedList<Component>()
-    // add spacing between actors.
-    while (getActorsWidth() + getMoreActorsWidth() > width) {
-      var actor: Component?
-      actor = actors.peekLast()
-      if (moreActors != null && actor === moreActors) {
-        actor = if (actors.size >= 2) {
-          actors[actors.size - 2]
-        } else {
-          null
-        }
-      }
-      if (actor != null) {
-        extraActors.addFirst(actor)
-        removeActor(actor)
-      }
-    }
-    return extraActors
-  }
-
-  /**
-   * Restores the extra actors in the actor panel.
-   */
-  protected fun restoreExtraActors(width: Int) {
-    if (actors == null || !actors.contains(moreActors)) {
-      return  // no extra actors are added.
-    }
-    for (actor in moreActors.getActors()) {
-      if (getMoreActorsWidth() + getActorsWidth() + actorsWidths[actor]!! < width) {
-        // the actor should be restored
-        moreActors.removeActor(actor)
-        removeActor(moreActors)
-        addActor(actor)
-        if (moreActors.isEmpty()) {
-          removeActor(moreActors)
-          break
-        } else {
-          addActor(moreActors)
-        }
-      } else {
-        break
-      }
-    }
-  }
-
-  /**
-   * Returns the more item client width.
-   * @return The more item client width.
-   */
-  protected fun getMoreActorsWidth(): Int {
-    return if (actors.contains(moreActors)) moreActors.element.getAttribute("offsetWidth").toInt() else 0
+  actor.element.setAttribute("className", "test")
+    this.addItem(actor)
   }
 }
