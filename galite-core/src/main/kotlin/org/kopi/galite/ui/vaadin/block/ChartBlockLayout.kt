4d4ce40062d8094150baf72d29a746a6eae983ac
hfazai <hf.fazai@gmail.com>
2021-02-23 15:23:02 +0100

    Block UI implementation

---
+++ galite-core/src/main/kotlin/org/kopi/galite/ui/vaadin/block/ChartBlockLayout.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2013-2020 kopiLeft Services SARL, Tunis TN
+ * Copyright (c) 1990-2020 kopiRight Managed Solutions GmbH, Wien AT
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.kopi.galite.ui.vaadin.block
+
+/**
+ * The chart block layout.
+ *
+ * @param col The number of columns.
+ * @param line The number of lines.
+ */
+class ChartBlockLayout(col: Int, line: Int) : AbstractBlockLayout(col, line), BlockLayout {
+
+  /**
+   * Can the block scroll ? If yes, draw a scroll bar
+   */
+  var hasScroll = false
+}
