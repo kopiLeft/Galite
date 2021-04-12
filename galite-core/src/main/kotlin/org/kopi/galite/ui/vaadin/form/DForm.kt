4b4700ab30d8921fc5c12d18178ecf2e89ab99bc
hfazai <hf.fazai@gmail.com>
2021-04-12 10:29:18 +0100

    FIX: resolve page selection in forms

--- galite-core/src/main/kotlin/org/kopi/galite/ui/vaadin/form/DForm.kt
+++ galite-core/src/main/kotlin/org/kopi/galite/ui/vaadin/form/DForm.kt
@@ -316,7 +316,7 @@ class DForm(model: VForm) : DWindow(model), UForm, FormListener {
       val pageNumber = block.pageNumber
       val blocks = vForm.blocks
       if (newAccess) {
-        // content.setEnabled(true, pageNumber) TODO
+        content.setEnabled(true, pageNumber)
       } else {
         // tab is visible (another visible block there?)
         for (i in blocks.indices) {
@@ -324,7 +324,7 @@ class DForm(model: VForm) : DWindow(model), UForm, FormListener {
             return
           }
         }
-        // content.setEnabled(false, pageNumber) TODO
+        content.setEnabled(false, pageNumber)
       }
     }
 
