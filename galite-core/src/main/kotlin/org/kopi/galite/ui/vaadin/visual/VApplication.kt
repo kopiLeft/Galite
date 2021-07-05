7b0ec222be2db75ca7c7bfa24fed5b77139e0bb7
Hichem Fazai <hf.fazai@gmail.com>
2021-07-05 19:22:30 +0100

    VaadinUI: allow the browser's feature to save the entered credentials (#366)
    * VaadinUI: allow the browser's feature to save the entered credentials
    
    * Null safety for getting current servlet object
    
    * Revert changes in VApplicationTestBase
    
    * Revert changes in VApplication
    
    * Null safety for getting current servlet object

--- galite-core/src/main/kotlin/org/kopi/galite/ui/vaadin/visual/VApplication.kt
+++ galite-core/src/main/kotlin/org/kopi/galite/ui/vaadin/visual/VApplication.kt
@@ -95,15 +95,15 @@ abstract class VApplication(override val registry: Registry) : VerticalLayout(),
   override val startupTime: Date = Date() // remembers the startup time
 
   init {
-    instance = this
-  }
-
-  override fun onAttach(attachEvent: AttachEvent) {
     className = "galite"
     // registry and locale initialization
     initialize()
     gotoWelcomeView()
     askAnswer = MessageListener.AWR_UNDEF
+    instance = this
+  }
+
+  override fun onAttach(attachEvent: AttachEvent) {
     currentUI = attachEvent.ui
   }
 
@@ -479,7 +479,7 @@ abstract class VApplication(override val registry: Registry) : VerticalLayout(),
    * @return The initialization parameter contained in the application descriptor file.
    */
   protected fun getInitParameter(key: String?): String? {
-    return VaadinServlet.getCurrent().getInitParameter(key)
+    return VaadinServlet.getCurrent()?.getInitParameter(key)
   }
 
   //---------------------------------------------------
