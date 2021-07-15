e42b4e0794a66669ff6bf419624d92026c8c3044
hfazai <hf.fazai@gmail.com>
2021-07-15 21:48:37 +0100

    VaadinUI: show error notification when generating a report without data

--- galite-core/src/main/kotlin/org/kopi/galite/ui/vaadin/visual/VWindowController.kt
+++ galite-core/src/main/kotlin/org/kopi/galite/ui/vaadin/visual/VWindowController.kt
@@ -21,6 +21,7 @@ import org.kopi.galite.common.Window
 import org.kopi.galite.preview.VPreviewWindow
 import org.kopi.galite.ui.vaadin.base.BackgroundThreadHandler
 import org.kopi.galite.ui.vaadin.base.BackgroundThreadHandler.access
+import org.kopi.galite.ui.vaadin.base.BackgroundThreadHandler.accessAndAwait
 import org.kopi.galite.ui.vaadin.base.BackgroundThreadHandler.startAndWait
 import org.kopi.galite.ui.vaadin.window.PopupWindow
 import org.kopi.galite.visual.VException
@@ -59,18 +60,21 @@ class VWindowController : WindowController() {
     val builder = getWindowBuilder(model)
     if (builder != null) {
       try {
-        access {
-          val view = builder.createWindow(model) as DWindow
-          view.run()
-          val application = getApplication()
-          if (application != null) {
-            if (model is VPreviewWindow
-              || model is VHelpViewer
-              || model is VMenuTree) {
-              showNotModalPopupWindow(view, model.getTitle())
-            } else {
-              application.addWindow(view, model.getTitle())
-            }
+        lateinit var view: DWindow
+
+        accessAndAwait {
+          view = builder.createWindow(model) as DWindow
+        }
+
+        view.run()
+        val application = getApplication()
+        if (application != null) {
+          if (model is VPreviewWindow
+            || model is VHelpViewer
+            || model is VMenuTree) {
+            showNotModalPopupWindow(view, model.getTitle())
+          } else {
+            application.addWindow(view, model.getTitle())
           }
         }
       } catch (e: VException) {
@@ -134,17 +138,17 @@ class VWindowController : WindowController() {
       val builder = getWindowBuilder(model)
       if (builder != null) {
         try {
-          access {
+          accessAndAwait {
             view = builder.createWindow(model) as DWindow
-            view!!.run()
-            val application = getApplication()
-            if (application != null) {
-              val popup = PopupWindow()
-              popup.isModal = true
-              popup.setContent(view!!)
-              popup.setCaption(model.getTitle()) // put popup title
-              popup.open()
-            }
+          }
+          view!!.run()
+          val application = getApplication()
+          if (application != null) {
+            val popup = PopupWindow()
+            popup.isModal = true
+            popup.setContent(view!!)
+            popup.setCaption(model.getTitle()) // put popup title
+            popup.open()
           }
         } catch (e: VException) {
           throw VRuntimeException(e.message, e)
