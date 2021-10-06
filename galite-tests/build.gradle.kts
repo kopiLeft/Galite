08fbdb8284b0b255567b3a01252fcb94c1cedf00
Hichem Fazai <hf.fazai@gmail.com>
2021-10-06 14:28:44 +0100

    Vbump: Vaadin 21.0.2 (#465)

--- galite-tests/build.gradle.kts
+++ galite-tests/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
   kotlin("jvm") apply true
   id("org.springframework.boot") version "2.4.0"
   id("io.spring.dependency-management") version "1.0.10.RELEASE"
-  id("com.vaadin") version "21.0.1"
+  id("com.vaadin") version "21.0.2"
   application
 }
 
