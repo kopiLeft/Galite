63db4e735bba77aa44676ef97d54d6bc05ae865e
MedAliMarz <servfrdali@yahoo.fr>
2021-11-16 15:09:25 +0100

    Add Unit test for VForm

--- galite-tests/src/test/kotlin/org/kopi/galite/tests/form/FormSampleTests.kt
+++ galite-tests/src/test/kotlin/org/kopi/galite/tests/form/FormSampleTests.kt
@@ -17,10 +17,16 @@
 package org.kopi.galite.tests.form
 
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 import org.junit.Assert.assertArrayEquals
 import org.junit.Test
 import org.kopi.galite.tests.ui.swing.JApplicationTestBase
+import org.kopi.galite.visual.form.VConstants
+import org.kopi.galite.visual.form.VForm
+
 
 class FormSampleTests: JApplicationTestBase() {
   @Test
@@ -40,5 +46,119 @@ class FormSampleTests: JApplicationTestBase() {
     assertArrayEquals(intArrayOf(1, 1, 2), formSample.tb4ToTestChangeBlockAccess.name.access)
     assertArrayEquals(intArrayOf(1, 1, 4), formSample.tb4ToTestChangeBlockAccess.password.access)
     assertArrayEquals(intArrayOf(1, 1, 2), formSample.tb4ToTestChangeBlockAccess.age.access)
+  }
+
+  @Test
+  fun `prepareForm test`() {
+    val formModel = FormSample().model
+
+    formModel.prepareForm()
+    assertNotNull(formModel.getActiveBlock())
+    assertNotNull(formModel.getActiveBlock()?.getActiveCommands())
+  }
+
+  @Test
+  fun `reset test`() {
+    val formModel = FormSample().model
+
+    formModel.reset()
+    assertTrue(formModel.blocks.all { it.getMode() == VConstants.MOD_QUERY })
+    assertNotNull(formModel.getActiveBlock())
+  }
+
+  @Test
+  fun `setCommandsEnabled test`() {
+    val form = FormSample()
+    val formModel = form.model
+
+    formModel.prepareForm() // internally will invoke setCommandsEnabled(true)
+    assertTrue(formModel.blocks.any{
+      it.getActiveCommands().isNotEmpty()
+    })
+  }
+
+  @Test
+  fun `gotoPage test`() {
+    val form = FormSample()
+    val formModel = form.model
+    val targetPage = 0
+
+    formModel.gotoPage(targetPage)
+    assertEquals(form.formBlocks[0].vBlock, formModel.getActiveBlock())
+  }
+
+  @Test
+  fun `gotoBlock test`() {
+    val form = FormSample()
+    val formModel = form.model
+    val targetBlock = 2
+
+    formModel.gotoBlock(formModel.getBlock(targetBlock))
+    assertEquals(form.formBlocks[targetBlock].vBlock, formModel.getActiveBlock())
+  }
+
+  @Test
+  fun `gotoNextBlock test`() {
+    val form = FormSample()
+    val formModel = form.model
+    val targetBlock = 1
+
+    formModel.gotoBlock(formModel.getBlock(targetBlock))
+    formModel.gotoNextBlock()
+    assertEquals(form.formBlocks[targetBlock + 1].vBlock, formModel.getActiveBlock())
+  }
+
+  @Test
+  fun `enterBlock test`() {
+    val form = FormSample()
+    val formModel = form.model
+
+    formModel.enterBlock()
+    assertEquals(form.formBlocks[0].vBlock, formModel.getActiveBlock())
+  }
+
+  @Test
+  fun `commitTrail test`() {
+    val form = FormSample()
+    val formModel = form.model
+
+    formModel.commitTrail()
+    assertTrue(formModel.blocks.all {
+      for (i in 0 until it.bufferSize) {
+        if (!it.isRecordTrailed(i))
+        {
+          false
+        }
+      }
+      true
+    })
+  }
+
+  @Test
+  fun `abortTrail test`() {
+    val form = FormSample()
+    val formModel = form.model
+
+    formModel.abortTrail()
+    assertTrue(formModel.blocks.all {
+      for (i in 0 until it.bufferSize) {
+        if (it.isRecordTrailed(i))
+        {
+          false
+        }
+      }
+      true
+    })
+  }
+
+  @Test
+  fun `getDefaultActor test`() {
+    val form = FormSample()
+    val formModel = form.model
+
+    assertNull(formModel.getDefaultActor(VForm.CMD_NEWITEM))
+    assertNotNull(formModel.getDefaultActor(VForm.CMD_AUTOFILL))
+    assertNull(formModel.getDefaultActor(VForm.CMD_EDITITEM))
+    assertNotNull(formModel.getDefaultActor(VForm.CMD_EDITITEM_S))
   }
 }
