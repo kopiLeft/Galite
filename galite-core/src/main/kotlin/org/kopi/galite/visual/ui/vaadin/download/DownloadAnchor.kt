718b74f4fb03adf976ce2c880cdf6708a63e677b
Hichem Fazai <hf.fazai@gmail.com>
2021-12-07 11:31:39 +0100

    VaadinUI: download produced file in server without showing a dialog (#514)
    * VaadinUI: download produced file in server without showing a dialog
    
    * Dont remove downloader component when donwload is triggered
    
    * Remove unecessary styling

--- galite-core/src/main/kotlin/org/kopi/galite/visual/ui/vaadin/download/DownloadAnchor.kt
+++ galite-core/src/main/kotlin/org/kopi/galite/visual/ui/vaadin/download/DownloadAnchor.kt
@@ -15,7 +15,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package org.kopi.galite.visual.ui.vaadin.visual
+package org.kopi.galite.visual.ui.vaadin.download
 
 import java.io.File
 import java.io.IOException
@@ -23,58 +23,26 @@ import java.io.InputStream
 import java.nio.file.Files
 import java.nio.file.Paths
 
-import org.kopi.galite.visual.ui.vaadin.base.LocalizedProperties
-
-import com.vaadin.flow.component.button.Button
-import com.vaadin.flow.component.dialog.Dialog
 import com.vaadin.flow.component.html.Anchor
-import com.vaadin.flow.component.html.Div
-import com.vaadin.flow.component.icon.Icon
-import com.vaadin.flow.component.icon.VaadinIcon
-import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.server.InputStreamFactory
 import com.vaadin.flow.server.StreamResource
 
 /**
- * A Dialog that allows a user to download a file produced in the server.
+ * An anchor component that allows to download a file produced in the server.
  *
  * @param file the file to download.
  * @param name the file name
  */
-class DownloaderDialog(file: File, name: String, locale: String): Dialog() {
-  private val downloadButton =
-    Button(LocalizedProperties.getString(locale, "downloadLabel"), Icon(VaadinIcon.DOWNLOAD_ALT))
-  private val closeButton = Button(LocalizedProperties.getString(locale, "CLOSE"), Icon(VaadinIcon.CLOSE_CIRCLE))
+open class DownloadAnchor(file: File, name: String): Anchor() {
 
   init {
     val href = StreamResource(name, InputStreamFactory {
       createFileInputStream(file.absolutePath)
     })
-    val download = Anchor(href, "")
-    val buttons = Div()
-    val title = Div()
 
-    title.className ="download-file-title"
-    buttons.className = "download-file-buttons"
-    download.element.setAttribute("download", true)
-    downloadButton.isDisableOnClick = true
+    element.setAttribute("download", true)
 
-    title.text = LocalizedProperties.getString(locale, "downloadText") + ": $name"
-    closeButton.addClickListener {
-      close()
-    }
-
-    download.add(downloadButton)
-    buttons.add(download, closeButton)
-    add(VerticalLayout(title, buttons))
-
-    downloadButton.style["visibility"] = "hidden"
-    closeButton.style["visibility"] = "hidden"
-
-    addAttachListener {
-      downloadButton.clickInClient()
-      closeButton.clickInClient()
-    }
+    setHref(href)
   }
 
   private fun createFileInputStream(path: String): InputStream? {
