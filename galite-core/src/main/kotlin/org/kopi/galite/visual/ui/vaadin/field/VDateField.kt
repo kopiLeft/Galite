43995d3cb98750a11e1ac78ad180aaca8570f933
hfazai <hf.fazai@gmail.com>
2022-02-25 12:32:21 +0100

    VaadinUI: Use setTimeout to set date field value only after the element is attached

--- galite-core/src/main/kotlin/org/kopi/galite/visual/ui/vaadin/field/VDateField.kt
+++ galite-core/src/main/kotlin/org/kopi/galite/visual/ui/vaadin/field/VDateField.kt
@@ -106,6 +106,8 @@ class DatePickerLight : AbstractField<TextField, String>(null), HasComponents,
 
   override fun setValue(value: String?) {
     textField.value = value
+    // Use setTimeout to set the value only after the element is attached
+    element.executeJs("setTimeout(function(){$0._inputValue = $1},0)", element, value)
   }
 
   override fun getValue(): String? {
