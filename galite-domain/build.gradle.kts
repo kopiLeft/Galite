47e732fc2ae29122bf73c9a2bf5520114e5a9bc9
hfazai <hf.fazai@gmail.com>
2022-03-24 09:58:15 +0100

    Galite 1.1.0-SNAPSHOT

--- galite-domain/build.gradle.kts
+++ galite-domain/build.gradle.kts
@@ -19,10 +19,63 @@ import org.kopi.galite.gradle.Versions
 
 plugins {
   kotlin("jvm") apply true
+  `maven-publish`
+  `java-library`
+  signing
 }
 
 dependencies {
   implementation("org.jetbrains.exposed", "exposed-core", Versions.EXPOSED)
   implementation("org.jetbrains.exposed", "exposed-jodatime", Versions.EXPOSED)
   implementation("org.jetbrains.exposed", "exposed-java-time", Versions.EXPOSED)
+}
+
+publishing {
+  publications {
+    create<MavenPublication>("Galite") {
+      pom {
+        description.set("Galite framework for building business applications.")
+        name.set(artifactId)
+        url.set("https://github.com/kopiLeft/Galite")
+        licenses {
+          license {
+            name.set("The GNU LESSER GENERAL PUBLIC LICENSE, Version 2.1")
+            url.set("https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt")
+          }
+        }
+        developers {
+          developer {
+            id.set("kopiLeft")
+            name.set("kopiLeft Team")
+            organization.set("kopiLeft Services")
+            organizationUrl.set("http://www.kopileft.com")
+            email.set("office@kopileft.com")
+          }
+          developer {
+            id.set("kopiRight")
+            name.set("kopiRight Team")
+            organization.set("kopiRight")
+            organizationUrl.set("http://www.kopiright.com")
+            email.set("office@kopiright.fr")
+          }
+        }
+        scm {
+          url.set("https://github.com/kopiLeft/Galite.git")
+          connection.set("scm:git:git://github.com/kopiLeft/Galite.git")
+          developerConnection.set("scm:git:git@github.com:kopiLeft/Galite.git")
+        }
+      }
+      from(components["java"])
+    }
+  }
+}
+
+signing {
+  useInMemoryPgpKeys(System.getenv("GPG_PRIVATE_KEY"), System.getenv("GPG_PRIVATE_PASSWORD"))
+  sign(publishing.publications)
+}
+
+java {
+  withSourcesJar()
+  withJavadocJar()
 }
